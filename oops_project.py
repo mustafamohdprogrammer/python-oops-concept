40fe06f60d4d1abe1993dec97b78c7e31fbcbbea
Mustafa <mohdmustafa969@gmail.com>
2025-11-05 15:47:16 +0530

    encapsulation and getter setter

--- oops_project.py
+++ oops_project.py
@@ -1,10 +1,24 @@
 class chatbook:
+
+    __user_id = 0
+
+
     def __init__(self):
-        self.__name = 'default' 
+        self.id = chatbook.__user_id
+        chatbook.__user_id += 1
+        self.__name = 'default' # private variable 
         self.username = ""
         self.password = ""
         self.logged_in = False
         # self.menu()
+
+    @staticmethod
+    def get_id():
+        return chatbook.__user_id
+        
+    @staticmethod
+    def set_id(val):
+        chatbook.__user_id = val
 
 
     def menu(self):
@@ -56,6 +70,12 @@ class chatbook:
         self.logged_in = False
         exit()     # <-- actually exit program
 
+    # getter setter
 
+    def get_name(self):
+        return self.__name
+
+    def set_name(self,value):
+        self.__name = value
 
 obj = chatbook()
--- rough.py
+++ rough.py
@@ -2,3 +2,18 @@ from oops_project import chatbook
 
 user1 = chatbook()
 print(user1._chatbook__name)  # Accessing the mangled private variable
+print(user1.id)
+
+
+user2 = chatbook()
+print(user2.id)
+
+
+chatbook.set_id(10)
+user3 = chatbook()
+print(user3.id)
+
+# getter and setter
+print(user1.get_name())
+user1.set_name('agent')
+print(user1.get_name())
